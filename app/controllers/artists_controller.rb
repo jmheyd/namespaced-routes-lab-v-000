c43d8dedb83f208f6b2c85763154e05443e55af6
jmheyd <heydjm@comcast.net>
2017-01-13 14:56:56 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,8 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    pref = Preference.first_or_create(artist_sort_order: "DESC")
+    pref.artist_sort_order = "DESC" if pref.artist_sort_order.blank?
+    @artists = Artist.all.order(name: pref.artist_sort_order)
   end
 
   def show
@@ -8,14 +10,15 @@ class ArtistsController < ApplicationController
   end
 
 
-
   def new
-    preference = Preference.all.first
-    if preference && !preference.allow_create_artists
+    pref = Preference.first
+    if pref && !pref.allow_create_artists
       redirect_to artists_path
+    else
+      @artist = Artist.new
     end
   end
-    
+
 
   def create
     @artist = Artist.new(artist_params)
